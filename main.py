2dcf2534fcd4f4757bf296c1ffc113f74c91ec18
TheSpatula <thespatulariansthespatula@gmail.com>
2026-01-03 13:00:38 +0100

    B: add FPS

--- main.py
+++ main.py
@@ -5,16 +5,21 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()  # note the ()
+    dt = 0
+
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
